6f9e6db34e8e06693051272743d15eb491f81745
antoniocastilla <38734176+antoniocastilla@users.noreply.github.com>
2018-04-25 20:41:54 +0200

    Update Catalogo.java

--- src/trabajo/entornos/Catalogo.java
+++ src/trabajo/entornos/Catalogo.java
@@ -5,7 +5,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-
+//prueba de edicion
 public class Catalogo extends JDialog{
     
     private JPanel jp1;
